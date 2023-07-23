646e4ab1b65f4d50f5588601492e5ee1b4848768
IDragnev <iddragnev@gmail.com>
2023-07-23 16:22:58 +0300

    Implement dig and autosym subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod process;
 mod name;
+mod procfs;
 
+use thiserror::*;
 use std::{
     env,
     error::Error,
@@ -21,6 +23,7 @@ struct Args {
 enum SubCommand {
     Autosym(AutosymArgs),
     Run(RunArgs),
+    Dig(DigArgs)
 }
 
 #[derive(FromArgs, PartialEq, Debug)]
@@ -42,6 +45,18 @@ struct RunArgs {
     exec_path: String,
 }
 
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand, name = "dig")]
+/// Shows information about an address in a process' address space
+struct DigArgs {
+    #[argh(option)]
+    /// the PID of the process whose memory space to examine
+    pid: u32,
+    #[argh(option)]
+    /// the address to look for
+    addr: u64,
+}
+
 fn main() {
     if let Err(e) = do_main() {
         eprintln!("Fatal error: {}", e);
@@ -53,11 +68,84 @@ fn do_main() -> Result<(), Box<dyn Error>> {
     match args.nested {
         SubCommand::Run(args) => cmd_run(args),
         SubCommand::Autosym(args) => cmd_autosym(args),
+        SubCommand::Dig(args) => cmd_dig(args),
+    }
+}
+
+#[derive(Error, Debug)]
+enum ProcessMappingsError {
+    #[error("parsing failed: {0}")]
+    Parse(String),
+}
+
+fn process_mappings<F, T>(pid: u32, f: F) -> Result<T, Box<dyn Error>>
+where
+    F: Fn(&Vec<procfs::Mapping<'_>>) -> Result<T, Box<dyn Error>>,
+{
+    let maps = std::fs::read_to_string(format!("/proc/{}/maps", pid))?;
+    match procfs::mappings(&maps) {
+        Ok((_, maps)) => f(&maps),
+        Err(e) => {
+            // parsing errors borrow the input, so we wouldn't be able
+            // to return it. to prevent that, format it early.
+            Err(Box::new(ProcessMappingsError::Parse(format!("{:?}", e))))
+        }
     }
 }
 
 fn cmd_autosym(args: AutosymArgs) -> Result<(), Box<dyn Error>> {
-    todo!()
+    fn analyze(mapping: &procfs::Mapping) -> Result<(), Box<dyn Error>> {
+        if mapping.deleted {
+            // skip deleted mappings
+            return Ok(());
+        }
+
+        let path = match mapping.source {
+            procfs::Source::File(path) => path,
+            _ => return Ok(()),
+        };
+
+        let contents = std::fs::read(path)?;
+        let file = match delf::File::parse_or_print_error(&contents) {
+            Some(x) => x,
+            _ => return Ok(()),
+        };
+
+        let section = match file
+            .section_headers
+            .iter()
+            .find(|sh| file.get_section_name(&contents, sh.name).unwrap() == b".text")
+        {
+            Some(section) => section,
+            _ => return Ok(()),
+        };
+
+        let textaddress = mapping.addr_range.start - mapping.offset + section.file_offset;
+        println!("add-symbol-file {:?} 0x{:?}", path, textaddress);
+
+        Ok(())
+    }
+
+    process_mappings(args.pid, |mappings| {
+        for mapping in mappings.iter().filter(|m| m.perms.x && m.source.is_file()) {
+            analyze(mapping)?;
+        }
+        Ok(())
+    })
+}
+
+fn cmd_dig(args: DigArgs) -> Result<(), Box<dyn Error>> {
+    let addr = delf::Addr(args.addr);
+
+    process_mappings(args.pid, |mappings| {
+        for mapping in mappings {
+            if mapping.addr_range.contains(&addr) {
+                println!("Found mapping: {:#?}", mapping);
+                return Ok(());
+            }
+        }
+        Ok(())
+    })
 }
 
 fn cmd_run(args: RunArgs) -> Result<(), Box<dyn Error>> {
