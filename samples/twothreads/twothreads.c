8adf43a258723ff8732b1c12c40968a7b600bd89
IDragnev <iddragnev@gmail.com>
2023-09-08 13:55:18 +0300

    Add sample program

---
+++ samples/twothreads/twothreads.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <pthread.h>
+#include <stdio.h>
+
+extern __thread int errno;
+
+__thread int val = 0;
+
+void* in_thread(void* unused) {
+    val += 1;
+
+    while (1) {
+        sleep(1);
+    }
+}
+
+int main() {
+    printf("errno = %d\n", errno);
+
+    pthread_t t1, t2;
+    pthread_create(&t1, NULL, in_thread, NULL);
+    pthread_create(&t2, NULL, in_thread, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+}
